507e418abacad7c7847720e8b2f1ad2ec5b2b178
keawade <keawade@gmail.com>
2015-11-20 11:19:33 -0600

    Now generating for all fields

--- content.js
+++ content.js
@@ -1,142 +1,593 @@
 var classList = [{
-  courseId: 'ACCT 212',
-  courseTitle: 'Principles of Accounting',
-  room: 'DB302',
-  days: 'MWF',
-  startTime: 1230,
-  endTime: 1320,
-  credits: 3
-},{
-  courseId: 'ART 104',
-  courseTitle: 'Drawing for Everyone',
-  room: 'EH 201',
-  days: 'MW',
-  startTime: 1330,
-  endTime: 1520,
-  credits: 2
-},{
-  courseId: 'ART 125',
-  courseTitle: 'Oil Painting',
-  room: 'EH 201',
-  days: 'MW',
-  startTime: 1530,
-  endTime: 1720,
-  credits: 2
-},{
-  courseId: 'BIOL 104',
-  courseTitle: 'Insects and You',
-  room: 'KC 135',
-  days: 'MWF',
-  startTime: 1130,
-  endTime: 1220,
-  credits: 3
-},{
-  courseId: 'BIOL 314',
-  courseTitle: 'Immunology',
-  room: 'KC 109',
-  days: 'MWF',
-  startTime: 930,
-  endTime: 1020,
-  credits: 3
-},{
-  courseId: 'BAUD 138',
-  courseTitle: 'Dynamics of Business',
-  room: 'DB 302',
-  days: 'MWF',
-  startTime: 1030,
-  endTime: 1120,
-  credits: 3
-},{
-  courseId: 'CHEM 151',
-  courseTitle: 'General Chemistry I',
-  room: 'KC 200',
-  days: 'MWF',
-  startTime: 1330,
-  endTime: 1420,
-  credits: 4
-},{
-  courseId: 'COMM 105',
-  courseTitle: 'Public Speaking',
-  room: 'DB 204',
-  days: 'MWF',
-  startTime: 1530,
-  endTime: 1620,
-  credits: 3
-},{
-  courseId: 'ENGL 112',
-  courseTitle: 'College Writing II',
-  room: 'DB 203',
-  days: 'MWF',
-  startTime: 800,
-  endTime: 920,
-  credits: 3
-},{
-  courseId: 'HHPT 216',
-  courseTitle: 'Athletic Injuries',
-  room: 'LL 212',
-  days: 'MWF',
-  startTime: 1030,
-  endTime: 1120,
-  credits: 3
-},{
-  courseId: 'MATH 184',
-  courseTitle: 'Calculus I',
-  room: 'KC 230',
-  days: 'MWF',
-  startTime: 1130,
-  endTime: 1220,
-  credits: 3
-},{
-  courseId: 'SOWK 115',
-  courseTitle: 'Introduction to Social Work',
-  room: 'DB 212',
-  days: 'MWF',
-  startTime: 1030,
-  endTime: 1120,
-  credits: 3
-},{
-  courseId: 'CPTR 245',
-  courseTitle: 'Web Development Basics',
-  room: 'DB 303',
-  days: 'TR',
-  startTime: 1530,
-  endTime: 1620,
-  credits: 3
+  "courseId": "BIOL 395",
+  "courseTitle": "Edible Plants",
+  "room": "KC 100",
+  "days": "MWF",
+  "startTime": 830,
+  "endTime": 920,
+  "credits": 3
+}, {
+  "courseId": "HIST 101",
+  "courseTitle": "A Brief History of the World",
+  "room": "DB 003",
+  "days": "TH",
+  "startTime": 700,
+  "endTime": 820,
+  "credits": 3
+}, {
+  "courseId": "IRR 101",
+  "courseTitle": "Wilderness Survival",
+  "room": "IR 102",
+  "days": "MTWHF",
+  "startTime": 1030,
+  "endTime": 1130,
+  "credits": 5
+}, {
+  "courseId": "PHIL 328",
+  "courseTitle": "Beliefs of Non-European Peoples",
+  "room": "DB 201",
+  "days": "MWF",
+  "startTime": 930,
+  "endTime": 1020,
+  "credits": 3
+}, {
+  "courseId": "ART 308",
+  "courseTitle": "Advanced Oil Painting",
+  "room": "EH 222",
+  "days": "TH",
+  "startTime": 900,
+  "endTime": 1020,
+  "credits": 3
+}, {
+  "courseId": "MATH 126",
+  "courseTitle": "Intermediate Algebra",
+  "room": "KC 103",
+  "days": "MWF",
+  "startTime": 1130,
+  "endTime": 1220,
+  "credits": 3
+}, {
+  "courseId": "PSYC 265",
+  "courseTitle": "Just Do It - The Psycology of Motivation",
+  "room": "DB 205",
+  "days": "TH",
+  "startTime": 1400,
+  "endTime": 1520,
+  "credits": 3
+}, {
+  "courseId": "BUAD 246",
+  "courseTitle": "Marketing to the Masses",
+  "room": "DB 303",
+  "days": "MWF",
+  "startTime": 930,
+  "endTime": 1020,
+  "credits": 3
+}, {
+  "courseId": "BUAD 256",
+  "courseTitle": "Intermediate Accounting",
+  "room": "DB 302",
+  "days": "MWF",
+  "startTime": 830,
+  "endTime": 920,
+  "credits": 3
+}, {
+  "courseId": "EDUC 186",
+  "courseTitle": "Memorable Storytelling",
+  "room": "DB 206",
+  "days": "TH",
+  "startTime": 1500,
+  "endTime": 1620,
+  "credits": 3
+}, {
+  "courseId": "MATH 221",
+  "courseTitle": "Calculus for Managers",
+  "room": "KC 103",
+  "days": "MWF",
+  "startTime": 730,
+  "endTime": 820,
+  "credits": 3
+}, {
+  "courseId": "CPTR 124",
+  "courseTitle": "Basic HTML and CSS",
+  "room": "DB 308",
+  "days": "TH",
+  "startTime": 2100,
+  "endTime": 2220,
+  "credits": 3
+}, {
+  "courseId": "CPTR 220",
+  "courseTitle": "Google Docs",
+  "room": "DB 307",
+  "days": "M",
+  "startTime": 1030,
+  "endTime": 1120,
+  "credits": 1
+}, {
+  "courseId": "CPTR 224",
+  "courseTitle": "Computer Science 1",
+  "room": "DB 205",
+  "days": "MWF",
+  "startTime": 2100,
+  "endTime": 2150,
+  "credits": 3
+}, {
+  "courseId": "CPTR 250",
+  "courseTitle": "Intro to Linux",
+  "room": "DB 205",
+  "days": "MWF",
+  "startTime": 2000,
+  "endTime": 2050,
+  "credits": 3
+}, {
+  "courseId": "BUAD 304",
+  "courseTitle": "Business law",
+  "room": "DB 302",
+  "days": "TH",
+  "startTime": 830,
+  "endTime": 920,
+  "credits": 3
+}, {
+  "courseId": "BIOL 100",
+  "courseTitle": "Introduction to Intro to Biology",
+  "room": "KC 132",
+  "days": "MWF",
+  "startTime": 930,
+  "endTime": 1020,
+  "credits": 3
+}, {
+  "courseId": "CHEM 332",
+  "courseTitle": "Science of Invisibility",
+  "room": "KC 230",
+  "days": "TH",
+  "startTime": 830,
+  "endTime": 1000,
+  "credits": 3
+}, {
+  "courseId": "CHEM 345",
+  "courseTitle": "The Chemical Makeup of James Bond",
+  "room": "KC 230",
+  "days": "MWF",
+  "startTime": 1130,
+  "endTime": 1220,
+  "credits": 3
+}, {
+  "courseId": "MATH 432",
+  "courseTitle": "Mathematical Theories of the Alien Kind",
+  "room": "KC 123",
+  "days": "MTWHF",
+  "startTime": 1330,
+  "endTime": 1420,
+  "credits": 5
+}, {
+  "courseId": "PHIL 101",
+  "courseTitle": "Introduction to Philosophy",
+  "room": "DB 214",
+  "days": "TH",
+  "startTime": 830,
+  "endTime": 1000,
+  "credits": 3
+}, {
+  "courseId": "PSYCH 312",
+  "courseTitle": "The Psychology of the Female Mind",
+  "room": "DB 003",
+  "days": "TH",
+  "startTime": 1400,
+  "endTime": 1520,
+  "credits": 3
+}, {
+  "courseId": "RELT 231",
+  "courseTitle": "Buddhist Beliefs",
+  "room": "DB 205",
+  "days": "MWF",
+  "startTime": 1130,
+  "endTime": 1220,
+  "credits": 3
+}, {
+  "courseId": "PSYCH 101",
+  "courseTitle": "Introduction to Psychology",
+  "room": "DB 002",
+  "days": "MW",
+  "startTime": 1430,
+  "endTime": 1520,
+  "credits": 3
+}, {
+  "courseId": "PSYCH 444",
+  "courseTitle": "The Psychology of Psychos",
+  "room": "DB 005",
+  "days": "MW",
+  "startTime": 1700,
+  "endTime": 1820,
+  "credits": 3
+}, {
+  "courseId": "CPTR 321",
+  "courseTitle": "Game development",
+  "room": "DB 307",
+  "days": "MWF",
+  "startTime": 930,
+  "endTime": 1020,
+  "credits": 3
+}, {
+  "courseId": "CPTR 425",
+  "courseTitle": "The Art of Hacking",
+  "room": "DB 307",
+  "days": "MWF",
+  "startTime": 1030,
+  "endTime": 1120,
+  "credits": 3
+}, {
+  "courseId": "PSYCH 212",
+  "courseTitle": "Cat Psychology",
+  "room": "DB 003",
+  "days": "MW",
+  "startTime": 1400,
+  "endTime": 1520,
+  "credits": 3
+}, {
+  "courseId": "PSYCH 320",
+  "courseTitle": "Psychology of SWAG",
+  "room": "DB 214",
+  "days": "MWF",
+  "startTime": 830,
+  "endTime": 920,
+  "credits": 3
+}, {
+  "courseId": "CPTR 223",
+  "courseTitle": "Small Network Management",
+  "room": "DB 205",
+  "days": "MWF",
+  "startTime": 930,
+  "endTime": 1020,
+  "credits": 3
+}, {
+  "courseId": "CPTR 347",
+  "courseTitle": "Mother of Circuit Boards",
+  "room": "DB 303",
+  "days": "MW",
+  "startTime": 1700,
+  "endTime": 1820,
+  "credits": 3
+}, {
+  "courseId": "RELB 314",
+  "courseTitle": "Hand Raising in Church",
+  "room": "DB 213",
+  "days": "MWF",
+  "startTime": 830,
+  "endTime": 920,
+  "credits": 3
+}, {
+  "courseId": "ACCT 212",
+  "courseTitle": "Principles of Accounting",
+  "room": "DB 302",
+  "days": "MWF",
+  "startTime": 1230,
+  "endTime": 1320,
+  "credits": 3
+}, {
+  "courseId": "BIOL 104",
+  "courseTitle": "Insects and You",
+  "room": "KC 135",
+  "days": "MWF",
+  "startTime": 1130,
+  "endTime": 1220,
+  "credits": 3
+}, {
+  "courseId": "BIOL 314",
+  "courseTitle": "Immunology",
+  "room": "KC 109",
+  "days": "MWF",
+  "startTime": 930,
+  "endTime": 1020,
+  "credits": 3
+}, {
+  "courseId": "CHEM 151",
+  "courseTitle": "General Chemistry I",
+  "room": "KC 200",
+  "days": "MWF",
+  "startTime": 1330,
+  "endTime": 1420,
+  "credits": 4
+}, {
+  "courseId": "CPTR 245",
+  "courseTitle": "Web Development Basics",
+  "room": "DB 303",
+  "days": "TR",
+  "startTime": 1530,
+  "endTime": 1620,
+  "credits": 3
+}, {
+  "courseId": "ENGL 112",
+  "courseTitle": "College Writing II",
+  "room": "DB 203",
+  "days": "MWF",
+  "startTime": 800,
+  "endTime": 920,
+  "credits": 3
+}, {
+  "courseId": "ART 125",
+  "courseTitle": "Oil Painting",
+  "room": "EH 201",
+  "days": "MW",
+  "startTime": 1530,
+  "endTime": 1720,
+  "credits": 2
+}, {
+  "courseId": "MATH 184",
+  "courseTitle": "Calculus I",
+  "room": "KC 230",
+  "days": "MWF",
+  "startTime": 1130,
+  "endTime": 1220,
+  "credits": 3
+}, {
+  "courseId": "BAUD 138",
+  "courseTitle": "Dynamics of Business",
+  "room": "DB 302",
+  "days": "MWF",
+  "startTime": 1030,
+  "endTime": 1120,
+  "credits": 3
+}, {
+  "courseId": "COMM 105",
+  "courseTitle": "Public Speaking",
+  "room": "DB 204",
+  "days": "MWF",
+  "startTime": 1530,
+  "endTime": 1620,
+  "credits": 3
+}, {
+  "courseId": "SOWK 115",
+  "courseTitle": "Introduction to Social Work",
+  "room": "DB 212",
+  "days": "MWF",
+  "startTime": 1030,
+  "endTime": 1120,
+  "credits": 3
+}, {
+  "courseId": "HHPT 216",
+  "courseTitle": "Athletic Injuries",
+  "room": "LL 212",
+  "days": "MWF",
+  "startTime": 1030,
+  "endTime": 1120,
+  "credits": 3
+}, {
+  "courseId": "MUHL304",
+  "courseTitle": "Choir",
+  "room": "EH 102",
+  "days": "MWF",
+  "startTime": 1230,
+  "endTime": 1320,
+  "credits": 1
+}, {
+  "courseId": "CPTR 101",
+  "courseTitle": "Making Fake Data",
+  "room": "DB 307",
+  "days": "TH",
+  "startTime": 1030,
+  "endTime": 1120,
+  "credits": 3
+}, {
+  "courseId": "EDUC 364",
+  "courseTitle": "Language Arts/Reading Methods",
+  "room": "DB 206",
+  "days": "MWF",
+  "startTime": 1030,
+  "endTime": 1120,
+  "credits": 3
+}, {
+  "courseId": "ENGL 111",
+  "courseTitle": "College Writing 1",
+  "room": "DB 003",
+  "days": "MWF",
+  "startTime": 930,
+  "endTime": 1020,
+  "credits": 2
+}, {
+  "courseId": "FNCE 215",
+  "courseTitle": "Personal Finance",
+  "room": "DB 304",
+  "days": "TH",
+  "startTime": 900,
+  "endTime": 1020,
+  "credits": 3
+}, {
+  "courseId": "MATH 111",
+  "courseTitle": "College Algebra",
+  "room": "KC 230",
+  "days": "MWF",
+  "startTime": 1330,
+  "endTime": 1420,
+  "credits": 3
+}, {
+  "courseId": "BIOL 111",
+  "courseTitle": "General Biology I",
+  "room": "KC 200",
+  "days": "MWF",
+  "startTime": 1030,
+  "endTime": 1120,
+  "credits": 4
+}, {
+  "courseId": "CHEM 151",
+  "courseTitle": "General Chemistry I",
+  "room": "KC 200",
+  "days": "MWF",
+  "startTime": 1330,
+  "endTime": 1420,
+  "credits": 4
+}, {
+  "courseId": "MATH 184",
+  "courseTitle": "Calculus I",
+  "room": "KC 230",
+  "days": "MWHF",
+  "startTime": 930,
+  "endTime": 1020,
+  "credits": 4
+}, {
+  "courseId": "EDUC 218",
+  "courseTitle": "Philosophy of Education",
+  "room": "DB 205",
+  "days": "MWF",
+  "startTime": 1030,
+  "endTime": 1120,
+  "credits": 3
+}, {
+  "courseId": "EDUC 245",
+  "courseTitle": "Physical Education in Elementary School",
+  "room": "DB 203",
+  "days": "TH",
+  "startTime": 1130,
+  "endTime": 1220,
+  "credits": 2
+}, {
+  "courseId": "MATH 351",
+  "courseTitle": "Abstract Algebra",
+  "room": "KC 240",
+  "days": "MW",
+  "startTime": 1330,
+  "endTime": 1450,
+  "credits": 3
+}, {
+  "courseId": "RELB 250",
+  "courseTitle": "Revelation",
+  "room": "DB 003",
+  "days": "MWF",
+  "startTime": 1030,
+  "endTime": 1120,
+  "credits": 3
+}, {
+  "courseId": "CPTR 487",
+  "courseTitle": "Network Programming",
+  "room": "DB 307",
+  "days": "MW",
+  "startTime": 1500,
+  "endTime": 1620,
+  "credits": 3
+}, {
+  "courseId": "HHPA 155",
+  "courseTitle": "Concepts of Wellness",
+  "room": "LL 100",
+  "days": "MWF",
+  "startTime": 830,
+  "endTime": 920,
+  "credits": 2
+}, {
+  "courseId": "HIST 157",
+  "courseTitle": "West and the World, Part 2",
+  "room": "DB 003",
+  "days": "TH",
+  "startTime": 930,
+  "endTime": 1020,
+  "credits": 2
+}, {
+  "courseId": "HIST 378",
+  "courseTitle": "Early Modern Europe",
+  "room": "DB 210",
+  "days": "MWF",
+  "startTime": 1130,
+  "endTime": 1220,
+  "credits": 3
+}, {
+  "courseId": "RELT 141",
+  "courseTitle": "Christian Beliefs",
+  "room": "DB 004",
+  "days": "MWF",
+  "startTime": 930,
+  "endTime": 1020,
+  "credits": 3
+}, {
+  "courseId": "BUAD 138",
+  "courseTitle": "Dynamics of Business",
+  "room": "DB 302",
+  "days": "MWF",
+  "startTime": 1300,
+  "endTime": 1450,
+  "credits": 3
+}, {
+  "courseId": "COMM 105",
+  "courseTitle": "Public Speaking",
+  "room": "DB 205",
+  "days": "MW",
+  "startTime": 1230,
+  "endTime": 1320,
+  "credits": 2
 }];
 
-function loadElements(){
+var majorsList = [
+  "Computer Science",
+  "Mathematics",
+  "Physics",
+  "Bio-Medical",
+  "Biology",
+  "Business",
+  "Education",
+  "Chemistry",
+  "Psychology",
+  "Philosophy"
+];
+
+function loadElements() {
   var button = document.getElementById('submit');
   button.addEventListener('click', generate, false);
 }
 
-function generate(event){
-  console.log("click!");
+function generate(event) {
+  var anchor = document.getElementById('anchor');
   var idStart = Number(document.getElementById('id-start').value);
   var idEnd = Number(document.getElementById('id-end').value);
-  var students = []
-  for(var i = 0; i <= (idEnd - idStart); i++){
-    students.push({
-      stuId: i + idStart,
-      nameLast: chance.last(),
-      nameFirst: chance.first(),
-      major: 'Computing',
-      credits: 42,
-      currentClasses: []
-    });
-    for(var j = 0; j < 4; j++){
-      var course = chance.integer({min: 0, max: classList.length-1});
-      students[students.length - 1].currentClasses.push(classList[course]);
+  if (isNaN(idStart) || isNaN(idEnd)) {
+    anchor.textContent = '';
+    var error = document.createElement('p');
+    error.textContent = "Error: Please check ID range";
+    anchor.appendChild(error);
+  } else {
+    var students = [];
+    for (var i = 0; i <= (idEnd - idStart); i++) {
+      students.push({
+        stuId: i + idStart,
+        nameLast: chance.last(),
+        nameFirst: chance.first(),
+        major: majorsList[chance.integer({
+          min: 0,
+          max: majorsList.length - 1
+        })],
+        credits: chance.integer({
+          min: 0,
+          max: 150
+        }),
+        currentClasses: []
+      });
     }
+
+    for (var j = 0; j < students.length - 1; j++) {
+      var numOfClasses = chance.integer({
+        min: 3,
+        max: 6
+      });
+      var prevClasses = [];
+      for (var k = 0; k < numOfClasses; k++) {
+        var found = false;
+        var potentialClass = chance.integer({
+          min: 0,
+          max: classList.length - 1
+        });
+        if (prevClasses.indexOf(potentialClass) == -1) {
+          prevClasses.push(potentialClass);
+          students[j].currentClasses.push(classList[potentialClass]);
+        } else {
+          k--;
+        }
+      }
+    }
+
+    var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(students));
+    var dl = document.createElement('a');
+    dl.href = "data:" + data;
+    dl.download = "data.json";
+    dl.textContent = "Download JSON";
+
+    anchor.textContent = '';
+    anchor.appendChild(dl);
   }
-
-  var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(students));
-  var dl = document.createElement('a');
-  dl.href = "data:" + data;
-  dl.download = "data.json";
-  dl.textContent = "Download JSON";
-
-  var anchor = document.getElementById('anchor');
-  anchor.appendChild(dl);
 }
 
 loadElements();
